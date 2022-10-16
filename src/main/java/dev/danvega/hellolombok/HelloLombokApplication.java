ba3958fdb6c5534956078d26bfa358727f10b144
Sasmita <sasmitapradhan01@gmail.com>
2022-10-16 06:58:42 -0400

    lombok commit

---
+++ src/main/java/dev/danvega/hellolombok/HelloLombokApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega.hellolombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/danvega/hellolombok/HelloLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.hellolombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
